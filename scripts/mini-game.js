f3e67987663eb18507c1d2f7ddb26d8dc76bc37b
Skipidar <segamania16@gmail.com>
2024-09-30 13:42:46 +0300

    Add files via upload

--- scripts/mini-game.js
+++ scripts/mini-game.js
@@ -1,46 +1,47 @@
-const MiniGame = (function() {
-    let gameTime = 60; // 1 минута для уровня
+const MiniGame = (function () {
+    let gameTime = 120; // 2 минуты для уровня
     let bees = [];
     let beeInterval;
     let gameTimerInterval;
     let lives = 3;
-    let gameCoins = 0;
-    let daisyCoins = 0; // Счетчик $Daisy
+    let gameCoins = 0; // Монеты не будут обнуляться между играми
     let currentLevel = 1;
     let isGameRunning = false;
     let ctx;
     let canvas;
-    let totalCoinsEarned = 0;
+    let totalCoinsEarned = 0; // Всего заработанных монет за игру
     let tickets = 200; // Начальное количество билетов для теста
-    let heartDropInterval;
 
     function init() {
         const startButton = document.getElementById('start-mini-game');
-        startButton.addEventListener('click', startCountdown); // Обратный отсчет перед игрой
+        startButton.addEventListener('click', startCountdown);
     }
 
     function startCountdown() {
+        const countdownOverlay = document.createElement('div');
+        countdownOverlay.id = 'countdown-overlay';
+        countdownOverlay.style.position = 'fixed';
+        countdownOverlay.style.top = '50%';
+        countdownOverlay.style.left = '50%';
+        countdownOverlay.style.transform = 'translate(-50%, -50%)';
+        countdownOverlay.style.fontSize = '5rem';
+        countdownOverlay.style.color = 'white';
+        countdownOverlay.style.zIndex = '9999';
+        document.body.appendChild(countdownOverlay);
+
         let countdown = 3;
-        const countdownElement = document.createElement('div');
-        countdownElement.id = 'countdown';
-        countdownElement.style.position = 'fixed';
-        countdownElement.style.top = '50%';
-        countdownElement.style.left = '50%';
-        countdownElement.style.transform = 'translate(-50%, -50%)';
-        countdownElement.style.fontSize = '64px';
-        countdownElement.style.color = 'white';
-        countdownElement.style.zIndex = '1000';
-        document.body.appendChild(countdownElement);
 
         const countdownInterval = setInterval(() => {
-            if (countdown > 1) {
-                countdownElement.textContent = countdown--;
-            } else if (countdown === 1) {
-                countdownElement.textContent = 'Поехали!';
-                countdown--;
-            } else {
+            countdownOverlay.textContent = countdown;
+            countdown--;
+
+            if (countdown < 0) {
+                countdownOverlay.textContent = 'Поехали!';
+            }
+
+            if (countdown < -1) {
                 clearInterval(countdownInterval);
-                document.body.removeChild(countdownElement);
+                countdownOverlay.remove();
                 startGame();
             }
         }, 1000);
@@ -70,9 +71,15 @@ const MiniGame = (function() {
             width: 100,
             height: 100,
             image: new Image(),
-            draw: function() {
-                ctx.drawImage(this.image, this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
-            }
+            draw: function () {
+                ctx.drawImage(
+                    this.image,
+                    this.x - this.width / 2,
+                    this.y - this.height / 2,
+                    this.width,
+                    this.height
+                );
+            },
         };
         flower.image.src = 'assets/images/PodsolnuhBEE.webp';
         flower.image.onload = () => {
@@ -84,10 +91,10 @@ const MiniGame = (function() {
         updateLives();
 
         bees = [];
-        gameTime = 60;
+        gameTime = 120;
         updateGameCoinCount();
-        updateDaisyCoinCount(); // Обновление $Daisy
 
+        // Музыка и спавн пчел в зависимости от уровня
         if (currentLevel === 1) {
             AudioManager.playOneLevelMusic();
         } else {
@@ -97,8 +104,6 @@ const MiniGame = (function() {
         const spawnInterval = currentLevel === 1 ? 1500 : 1000;
         beeInterval = setInterval(() => spawnBee(currentLevel), spawnInterval);
 
-        heartDropInterval = setInterval(dropHeart, 20000); // Падает сердце каждые 20 секунд
-
         gameTimerInterval = setInterval(() => {
             gameTime--;
             document.getElementById('game-timer').textContent = formatTime(gameTime);
@@ -106,18 +111,18 @@ const MiniGame = (function() {
             if (gameTime <= 0) {
                 if (currentLevel === 1) {
                     currentLevel = 2;
-                    gameTime = 60;
+                    gameTime = 120;
                     AudioManager.pauseOneLevelMusic();
                     AudioManager.playElectricChaosMusic();
                     clearInterval(beeInterval);
                     beeInterval = setInterval(() => spawnBee(currentLevel), 1000);
-                    updateBackgroundForHardMode();
                 } else {
                     endGame();
                 }
             }
         }, 1000);
 
+        // Основной игровой цикл
         function gameLoop() {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             flower.draw();
@@ -131,22 +136,6 @@ const MiniGame = (function() {
         canvas.addEventListener('click', handleCanvasClick);
     }
 
-    function dropHeart() {
-        const heart = {
-            x: Math.random() * canvas.width,
-            y: -50,
-            width: 50,
-            height: 50,
-            speed: 2,
-            image: new Image(),
-            draw: function() {
-                ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-            }
-        };
-        heart.image.src = 'assets/images/heart.png';
-        bees.push(heart);
-    }
-
     function spawnBee(level) {
         const size = Math.floor(Math.random() * 60) + 40;
         const speed = level === 1 ? 2 : 3.5;
@@ -154,10 +143,22 @@ const MiniGame = (function() {
 
         const side = Math.floor(Math.random() * 4);
         switch (side) {
-            case 0: x = Math.random() * canvas.width; y = -size; break;
-            case 1: x = canvas.width + size; y = Math.random() * canvas.height; break;
-            case 2: x = Math.random() * canvas.width; y = canvas.height + size; break;
-            case 3: x = -size; y = Math.random() * canvas.height; break;
+            case 0:
+                x = Math.random() * canvas.width;
+                y = -size;
+                break;
+            case 1:
+                x = canvas.width + size;
+                y = Math.random() * canvas.height;
+                break;
+            case 2:
+                x = Math.random() * canvas.width;
+                y = canvas.height + size;
+                break;
+            case 3:
+                x = -size;
+                y = Math.random() * canvas.height;
+                break;
         }
 
         const bee = {
@@ -167,14 +168,20 @@ const MiniGame = (function() {
             height: size,
             speed: speed,
             image: new Image(),
-            draw: function() {
-                ctx.drawImage(this.image, this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+            draw: function () {
+                ctx.drawImage(
+                    this.image,
+                    this.x - this.width / 2,
+                    this.y - this.height / 2,
+                    this.width,
+                    this.height
+                );
             },
-            move: function(flower) {
+            move: function (flower) {
                 const angle = Math.atan2(flower.y - this.y, flower.x - this.x);
                 this.x += Math.cos(angle) * this.speed;
                 this.y += Math.sin(angle) * this.speed;
-            }
+            },
         };
         bee.image.src = 'assets/images/Bee.webp';
         bees.push(bee);
@@ -186,28 +193,20 @@ const MiniGame = (function() {
         const yClick = event.clientY - rect.top;
 
         bees.forEach((bee, index) => {
-            if (xClick >= bee.x - bee.width / 2 && xClick <= bee.x + bee.width / 2 &&
-                yClick >= bee.y - bee.height / 2 && yClick <= bee.y + bee.height / 2) {
+            if (
+                xClick >= bee.x - bee.width / 2 &&
+                xClick <= bee.x + bee.width / 2 &&
+                yClick >= bee.y - bee.height / 2 &&
+                yClick <= bee.y + bee.height / 2
+            ) {
                 bees.splice(index, 1);
-                if (bee.image.src.includes('goldcoin.webp')) {
-                    daisyCoins += 10; // Добавление $Daisy
-                    updateDaisyCoinCount();
-                    AudioManager.playMoneySound(); // Звук поимки монеты
-                } else if (bee.image.src.includes('heart.png')) {
-                    if (lives < 3) {
-                        lives++;
-                        updateLives();
-                        AudioManager.playHeartSound(); // Звук восстановления жизни
-                    }
-                } else {
-                    gameCoins += 1;
-                    totalCoinsEarned += 1;
-                    updateGameCoinCount();
-                    AudioManager.playClickSound();
+                gameCoins += 1;
+                totalCoinsEarned += 1;
+                updateGameCoinCount();
+                AudioManager.playClickSound();
 
-                    if (navigator.vibrate) {
-                        navigator.vibrate(100);
-                    }
+                if (navigator.vibrate) {
+                    navigator.vibrate(100);
                 }
             }
         });
@@ -232,28 +231,27 @@ const MiniGame = (function() {
                 }
             }
 
-            if (bee.x < -bee.width || bee.x > canvas.width + bee.width ||
-                bee.y < -bee.height || bee.y > canvas.height + bee.height) {
+            if (
+                bee.x < -bee.width ||
+                bee.x > canvas.width + bee.width ||
+                bee.y < -bee.height ||
+                bee.y > canvas.height + bee.height
+            ) {
                 bees.splice(i, 1);
             }
         }
     }
 
-    function updateBackgroundForHardMode() {
-        const gameScreen = document.getElementById('protect-flower-game');
-        gameScreen.style.backgroundColor = 'rgba(255, 0, 0, 0.5)'; // Заливка красным
-    }
-
     function shakeScreen() {
         const gameScreen = document.getElementById('protect-flower-game');
         gameScreen.style.animation = 'shake 0.1s';
-        setTimeout(() => gameScreen.style.animation = '', 100);
+        setTimeout(() => (gameScreen.style.animation = ''), 100);
     }
 
     function flashFlower() {
         const flower = document.getElementById('game-canvas');
         flower.style.filter = 'brightness(0.5)';
-        setTimeout(() => flower.style.filter = '', 100);
+        setTimeout(() => (flower.style.filter = ''), 100);
     }
 
     function isColliding(obj1, obj2) {
@@ -280,16 +278,14 @@ const MiniGame = (function() {
         document.getElementById('game-coin-count').textContent = gameCoins;
     }
 
-    function updateDaisyCoinCount() {
-        document.getElementById('daisy-coin-count').textContent = daisyCoins;
-    }
-
     function updateTicketCount() {
         document.getElementById('ticket-count').textContent = tickets;
     }
 
     function formatTime(seconds) {
-        const m = Math.floor(seconds / 60).toString().padStart(2, '0');
+        const m = Math.floor(seconds / 60)
+            .toString()
+            .padStart(2, '0');
         const s = (seconds % 60).toString().padStart(2, '0');
         return `${m}:${s}`;
     }
@@ -297,7 +293,6 @@ const MiniGame = (function() {
     function endGame() {
         clearInterval(beeInterval);
         clearInterval(gameTimerInterval);
-        clearInterval(heartDropInterval); // Останавливаем появление сердец
         AudioManager.pauseOneLevelMusic();
         AudioManager.playElectricChaosMusic();
 
@@ -312,7 +307,7 @@ const MiniGame = (function() {
         resultModal.style.padding = '20px';
         resultModal.innerHTML = `
             <h2>Игра окончена!</h2>
-            <p>Вы заработали ${totalCoinsEarned} Coin и ${daisyCoins} $Daisy.</p>
+            <p>Вы заработали ${totalCoinsEarned} Coin.</p>
             <button class="replay-btn">
                 <img src="assets/images/Ticket.webp" alt="Ticket" class="ticket-icon"> Повторим? (${tickets} Tickets)
             </button>
@@ -325,7 +320,7 @@ const MiniGame = (function() {
 
         replayButton.addEventListener('click', () => {
             resultModal.remove();
-            startCountdown(); // Снова запускаем обратный отсчет перед игрой
+            startGame();
         });
 
         exitButton.addEventListener('click', () => {
@@ -334,7 +329,6 @@ const MiniGame = (function() {
             gameScreen.style.display = 'none';
             document.querySelector('.game-container').style.display = 'flex';
             totalCoinsEarned = 0;
-            daisyCoins = 0;
             currentLevel = 1;
         });
 
@@ -342,7 +336,7 @@ const MiniGame = (function() {
     }
 
     return {
-        init
+        init,
     };
 })();
 
